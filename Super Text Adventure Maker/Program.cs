4a3306107cbca0d1ba6a78543dde5b23e4d9743a
Isaac Lyman <isaaclyman@gmail.com>
2017-03-25 08:44:20 -0600

    Initial solution creation

---
+++ Super Text Adventure Maker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Super_Text_Adventure_Maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
